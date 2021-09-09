aa02b8529ac1aed27c48270b7236b4df3862d1ea
JohnRamberger <rambergerjohn@gmail.com>
2021-09-08 20:14:03 -0400

    updated files

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 var HTTPS = require('https');
-var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
@@ -12,7 +11,8 @@ function respond() {
     postMessage("test message");
     this.res.end();
   } else {
-    console.log("don't care");
+    console.log("message sent from ");
+    console.log(request)
     this.res.writeHead(200);
     this.res.end();
   }
@@ -34,21 +34,21 @@ function postMessage(_message) {
     "text": botResponse
   };
 
-  console.log('sending ' + botResponse + ' to ' + botID);
+  console.log(`sending "${botResponse}" to botID`);
 
   botReq = HTTPS.request(options, function (res) {
     if (res.statusCode == 202) {
       //neat
     } else {
-      console.log('rejecting bad status code ' + res.statusCode);
+      console.log(`rejecting bad status code ${res.statusCode}`);
     }
   });
 
   botReq.on('error', function (err) {
-    console.log('error posting message ' + JSON.stringify(err));
+    console.log(`error posting message ${JSON.stringify(err)}`);
   });
   botReq.on('timeout', function (err) {
-    console.log('timeout posting message ' + JSON.stringify(err));
+    console.log(`timeout posting message ${JSON.stringify(err)}`);
   });
   botReq.end(JSON.stringify(body));
 }
