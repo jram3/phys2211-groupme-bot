0b4e39115aa06205d483c6681ade7c58ecd741a5
JohnRamberger <rambergerjohn@gmail.com>
2021-09-08 20:29:02 -0400

    updated bot

--- bot.js
+++ bot.js
@@ -3,54 +3,64 @@ var HTTPS = require('https');
 var botID = process.env.BOT_ID;
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-    botRegex = /^\/cool guy$/;
+	var request = JSON.parse(this.req.chunks[0]),
+		botRegex = /^\/cool guy$/;
 
-  if (request.text && botRegex.test(request.text)) {
+	if (request.text) {
+        //higher the if statement, higher the priority
+        var msg;
+        if(request.text.toLowerCase().includes("test1234")) {
+            msg = "message contains test1234";
+        }
+        else if(request.text.toLowerCase().includes("test123")) {
+            msg = "message contains test1234";
+        }
+
+        if(msg) {
+            this.res.writeHead(200);
+            postMessage(msg);
+            this.res.end();
+            return;
+        }
+	}
+    console.log("message sent from " + request.name + " in " + request.group_id);
     this.res.writeHead(200);
-    postMessage("test message");
     this.res.end();
-  } else {
-    console.log("message sent from ");
-    console.log(request)
-    this.res.writeHead(200);
-    this.res.end();
-  }
 }
 
 function postMessage(_message) {
-  var botResponse, options, body, botReq;
+	var botResponse, options, body, botReq;
 
-  botResponse = _message;
+	botResponse = _message;
 
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
+	options = {
+		hostname: 'api.groupme.com',
+		path: '/v3/bots/post',
+		method: 'POST'
+	};
 
-  body = {
-    "bot_id": botID,
-    "text": botResponse
-  };
+	body = {
+		"bot_id": botID,
+		"text": botResponse
+	};
 
-  console.log('sending ' + botResponse + ' to botID');
+	console.log('sending ' + botResponse + ' to botID');
 
-  botReq = HTTPS.request(options, function (res) {
-    if (res.statusCode == 202) {
-      //neat
-    } else {
-      console.log('rejecting bad status code ' + res.statusCode);
-    }
-  });
+	botReq = HTTPS.request(options, function (res) {
+		if (res.statusCode == 202) {
+			//neat
+		} else {
+			console.log('rejecting bad status code ' + res.statusCode);
+		}
+	});
 
-  botReq.on('error', function (err) {
-    console.log('error posting message ' + JSON.stringify(err));
-  });
-  botReq.on('timeout', function (err) {
-    console.log('timeout posting message ' + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
+	botReq.on('error', function (err) {
+		console.log('error posting message ' + JSON.stringify(err));
+	});
+	botReq.on('timeout', function (err) {
+		console.log('timeout posting message ' + JSON.stringify(err));
+	});
+	botReq.end(JSON.stringify(body));
 }
 
 
---
+++ example_request.js
@@ -0,0 +1,13 @@
+var example_request = {
+    avatar_url: 'https://i.groupme.com/800x825.jpeg.66a5cae2e98540cea8dbf14d21d9c9f4',
+    created_at: 1631146698,
+    group_id: '70766269',
+    id: '163114669834794661',
+    name: 'test bot',
+    sender_id: '860105',
+    sender_type: 'bot',
+    source_guid: '5b50d4a0f3310139ac9c5282496803c4',
+    system: false,
+    text: 'test message',
+    user_id: '860105'
+}
